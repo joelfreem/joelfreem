eede003595e94f6715e6e62da79619e385589562
joelfreem <joelfreem@gmail.com>
2026-09-16 21:59:41 -0400

    Likewise

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,6 +4,7 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("pain and suffering!"));
+    System.out.println("Suffering and Pain-Jo'el");
 
 
     for (int i = 1; i <= 5; i++) {
