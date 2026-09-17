d7b68bb5d6bc66dc43bc6676a10146706288dd01
brody6208 <brody6208@gmail.com>
2026-09-16 21:52:04 -0400

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,7 +3,7 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("pain!"));
+    IO.println(String.format("pain and suffering!"));
 
 
     for (int i = 1; i <= 5; i++) {
